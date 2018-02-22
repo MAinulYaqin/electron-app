9d2616fb4ae007a1ddcfe2d628273d33dda64150
MAinulYaqin <ainul.y9b@gmail.com>
2018-02-22 14:00:41 +0700

    adding new controllers

--- controllers/insert.js
+++ controllers/insert.js
@@ -1,3 +1,5 @@
+// @ts-check
+
 var mysql = require('mysql');
 var config = require('../assets/config');
 var connection = mysql.createConnection(config.mysql_config);
---
+++ controllers/show.js
@@ -0,0 +1,34 @@
+// @ts-check
+
+var mysql = require('mysql');
+var config = require('../assets/config');
+var connection = mysql.createConnection(config.mysql_config);
+
+module.exports = {
+    /**
+     * @param {String} db
+        table's name you want to show
+     */
+    showAll : function (db) {
+        connection.query(`SELECT * FROM ${db}`, function (err, result) {
+            if (err) throw Error;
+
+            console.log(result)
+        })
+    },
+    single : function (db) {
+        var id;
+        document.getElementById('someButton').addEventListener('click', function (e) {
+            e.preventDefault();
+
+            console.log(this.id);
+            id = this.id;
+        });
+        connection.query(`SELECT * FROM ${db} WHERE id= ${id}`, function (err, result) {
+            if (err) throw Error;
+
+            console.log(result);
+            console.log('=======================');
+        })
+    }
+}
